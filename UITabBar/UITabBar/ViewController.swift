49dfc409ec0dcbfa664371b0f5d9fd2c6955de13
Rakib Hasan <33759162+rakib654258@users.noreply.github.com>
2019-09-20 23:29:31 +0600

    UITabBarController

---
+++ UITabBar/UITabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBar
+//
+//  Created by Rakib Hasan Shayan on 23/7/19.
+//  Copyright © 2019 Rakib Hasan Shayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
